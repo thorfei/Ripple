e81d233908cc937479f98d41676ae55642f9a3eb
hongfei <codefei@126.com>
2014-06-23 13:43:24 +0800

    Ripple
    a example for studying ios app develop

---
+++ Ripple/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ripple
+//
+//  Created by Meng To on 29/11/13.
+//  Copyright (c) 2013 Meng To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ripple/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ripple
+//
+//  Created by Meng To on 29/11/13.
+//  Copyright (c) 2013 Meng To. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
